2573c8de59c920938ba4a983419a10e2d475e7a2
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-06-15 16:51:51 +0200

    Init counter oop project

---
+++ cours-javascript/06-projet-POO/01-compteur/app.js
@@ -0,0 +1,17 @@
+const firstCounter = document.querySelector('.first-counter');
+
+class Counter {
+  constructor(element, value) {
+    // élément compteur
+    // valeur
+    // élément pour afficher la valeur dans le DOM
+    // 3 boutons
+    // faire les event listeners sur les boutons
+  }
+
+  // binder les 3 fonctions à "this"
+
+  // incrémenter
+  // décrémenter
+  // reset
+}
