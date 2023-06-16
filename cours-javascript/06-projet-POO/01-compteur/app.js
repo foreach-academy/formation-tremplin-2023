2a047eb8eeec7921439db41e38646f7a4c148685
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-06-16 09:58:50 +0200

    Add counter project

--- cours-javascript/06-projet-POO/01-compteur/app.js
+++ cours-javascript/06-projet-POO/01-compteur/app.js
@@ -1,16 +1,66 @@
-const firstCounter = document.querySelector('.first-counter');
-
 class Counter {
-  constructor(element, value) {
-    // élément compteur
+  constructor(title, value) {
+    // titre
+    this.title = title;
+    // valeur par défaut pour le reset
+    this.defaultValue = value;
     // valeur
+    this.value = value;
+    // élément compteur
+    this.element = this.generateCounter();
     // élément pour afficher la valeur dans le DOM
+    this.valueDOM = this.element.querySelector('.value');
+
     // 3 boutons
+    this.incrementBtn = this.element.querySelector('.increment');
+    this.decrementBtn = this.element.querySelector('.decrement');
+    this.resetBtn = this.element.querySelector('.reset');
+
     // binder les 3 fonctions à "this"
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    this.increment = this.increment.bind(this);
+
     // faire les event listeners sur les boutons
+    this.decrementBtn.addEventListener('click', this.decrement);
+    this.resetBtn.addEventListener('click', this.reset);
+    this.incrementBtn.addEventListener('click', this.increment);
+  }
+
+  generateCounter() {
+    const element = document.createElement('section');
+    element.innerHTML = `
+    <h2>${this.title}</h2>
+    <span class="value">${this.value}</span>
+    <div class="btn-container">
+      <button class="decrement">Décrémenter</button>
+      <button class="reset">Reset</button>
+      <button class="increment">Incrémenter</button>
+    </div>
+    `;
+    document.body.append(element);
+    return element;
+  }
+
+  // décrémenter
+  decrement() {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+  }
+
+  // reset
+  reset() {
+    this.value = this.defaultValue;
+    this.valueDOM.textContent = this.value;
   }
 
   // incrémenter
-  // décrémenter
-  // reset
+  increment() {
+    this.value++;
+    this.valueDOM.textContent = this.value;
+  }
 }
+
+const firstCounter = new Counter('Premier compteur', 100);
+const secondCounter = new Counter('Second compteur', 200);
+const thirdCounter = new Counter('Troisième compteur', 300);
