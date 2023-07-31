0e69bf55f981714eadd1a13fdfd7238d48de9d90
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-07-31 12:14:24 +0200

    Add not null clause

--- cours-node-express/03-projets/04-store-api/database.sql
+++ cours-node-express/03-projets/04-store-api/database.sql
@@ -4,8 +4,8 @@ CREATE TABLE products(
   product_id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   price NUMERIC(10, 2) NOT NULL,
-  featured BOOLEAN DEFAULT false,
-  rating NUMERIC(3, 1) DEFAULT 4.5,
+  featured BOOLEAN NOT NULL DEFAULT false,
+  rating NUMERIC(2, 1) NOT NULL DEFAULT 4.5,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   company VARCHAR(255) NOT NULL CHECK (company IN ('ikea', 'liddy', 'caressa', 'marcos'))
 );
